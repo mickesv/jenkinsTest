ae7909da141bfe3f3ff6ab10ee5b85ed38d471b7
Mikael Svahnberg <Mikael.Svahnberg@gmail.com>
2016-06-28 13:32:16 +0200

    makefile

--- main.cc
+++ main.cc
@@ -6,3 +6,13 @@ int main(void) {
   cout << "Hello World" << endl;
   return;
 }
+
+
+/*
+
+FROM jenkins
+USER root
+RUN apt-get update && apt-get install -y build-essential cppcheck
+USER jenkins
+
+*/
